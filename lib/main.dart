680d29e62202737a582574131d522e6655090e99
kysth0707 <kysth0707@naver.com>
2022-09-24 10:53:12 +0900

    feat : Two page is available now.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
+var WebPage = 0;
+
 void main() {
-  runApp(const MyApp());
+  // runApp(const MyApp());
+  runApp(MaterialApp(home: MyApp(),));
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -26,13 +30,69 @@ class MyApp extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Icon(Icons.music_note),
-              Icon(Icons.mail),
-              Icon(Icons.contact_page)
+              IconButton(onPressed: () {
+                WebPage = 0;
+                Navigator.push(context, MaterialPageRoute(builder: (context) => MusicSearch()),);
+              }, icon: Icon(Icons.music_note)),
+              IconButton(onPressed: () {
+                WebPage = 1;
+              }, icon: Icon(Icons.dynamic_feed)),
+              IconButton(onPressed: () {
+                WebPage = 2;
+              }, icon: Icon(Icons.dvr_rounded)),
             ],
           ),
         ),
       )
+    );
+  }
+}
+
+class MusicSearch extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(scaffoldBackgroundColor: Colors.black87),
+        home: Scaffold(
+          appBar: AppBar(
+              backgroundColor: Colors.black45,
+              title: Text('SLY   -   Save Listen Youtube',
+                  style: TextStyle(fontSize: 20))
+          ),
+          body: Row(
+            children: <Widget>[
+              SizedBox(width: 20),
+              Expanded(child:
+                TextField(decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 1, color: Colors.white)),
+                  ), style: TextStyle(color: Colors.white)
+                ),
+              ),
+              SizedBox(width: 20),
+              ElevatedButton.icon( onPressed: () { }, icon: Icon(Icons.search, size: 18), label: Text("Search"), ),
+              SizedBox(width: 20),
+            ],
+          ),
+
+          bottomNavigationBar: BottomAppBar(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(onPressed: () {
+                  WebPage = 0;
+                }, icon: Icon(Icons.music_note)),
+                IconButton(onPressed: () {
+                  WebPage = 1;
+                }, icon: Icon(Icons.dynamic_feed)),
+                IconButton(onPressed: () {
+                  WebPage = 2;
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()),);
+                }, icon: Icon(Icons.dvr_rounded)),
+              ],
+            ),
+          ),
+        ),
     );
   }
 }
