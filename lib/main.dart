464d7bee16afb941fa81ce7247080da12ab7314c
kysth0707 <kysth0707@naver.com>
2022-09-24 22:49:10 +0900

    feat : list view test

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,7 @@ class MyApp extends StatelessWidget {
               }, icon: Icon(Icons.music_note)),
               IconButton(onPressed: () {
                 WebPage = 1;
+                Navigator.push(context, MaterialPageRoute(builder: (context) => MusicList()),);
               }, icon: Icon(Icons.dynamic_feed)),
               IconButton(onPressed: () {
                 WebPage = 2;
@@ -52,47 +53,102 @@ class MusicSearch extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(scaffoldBackgroundColor: Colors.black87),
-        home: Scaffold(
-          appBar: AppBar(
-              backgroundColor: Colors.black45,
-              title: Text('SLY   -   Save Listen Youtube',
-                  style: TextStyle(fontSize: 20))
-          ),
-          body: Row(
-            children: <Widget>[
-              SizedBox(width: 20),
-              Expanded(child:
-                TextField(decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(width: 1, color: Colors.white)),
-                  ), style: TextStyle(color: Colors.white)
-                ),
-              ),
-              SizedBox(width: 20),
-              ElevatedButton.icon( onPressed: () { }, icon: Icon(Icons.search, size: 18), label: Text("Search"), ),
-              SizedBox(width: 20),
+      theme: ThemeData(scaffoldBackgroundColor: Colors.black87),
+      home: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.black45,
+            title: Text('SLY   -   Save Listen Youtube',
+                style: TextStyle(fontSize: 20))
+        ),
+        body: Row(
+          children: <Widget>[
+            SizedBox(width: 20),
+            Expanded(child:
+            TextField(decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(width: 1, color: Colors.white)),
+            ), style: TextStyle(color: Colors.white)
+            ),
+            ),
+            SizedBox(width: 20),
+            ElevatedButton.icon( onPressed: () { }, icon: Icon(Icons.search, size: 18), label: Text("Search"), ),
+            SizedBox(width: 20),
+          ],
+        ),
+
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(onPressed: () {
+                WebPage = 0;
+              }, icon: Icon(Icons.music_note)),
+              IconButton(onPressed: () {
+                WebPage = 1;
+                Navigator.push(context, MaterialPageRoute(builder: (context) => MusicList()),);
+              }, icon: Icon(Icons.dynamic_feed)),
+              IconButton(onPressed: () {
+                WebPage = 2;
+                Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()),);
+              }, icon: Icon(Icons.dvr_rounded)),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
 
-          bottomNavigationBar: BottomAppBar(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+class MusicList extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(scaffoldBackgroundColor: Colors.black87),
+      home: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.black45,
+            title: Text('SLY   -   Save Listen Youtube',
+                style: TextStyle(fontSize: 20))
+        ),
+        body: ListView.builder(itemCount: 10,
+          padding: EdgeInsets.all(10),
+          itemBuilder: (BuildContext context, int index){
+            return Row(
               children: [
-                IconButton(onPressed: () {
-                  WebPage = 0;
-                }, icon: Icon(Icons.music_note)),
-                IconButton(onPressed: () {
-                  WebPage = 1;
-                }, icon: Icon(Icons.dynamic_feed)),
-                IconButton(onPressed: () {
-                  WebPage = 2;
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()),);
-                }, icon: Icon(Icons.dvr_rounded)),
+                Icon(Icons.music_video, color: Colors.white,),
+                Container(
+                  height: 50,
+                  child: Center(
+                    child: Text('ㅎㅇ ${index}',
+                      style: TextStyle(color: Colors.white),),
+                  )
+                )
               ],
-            ),
+            );
+          }),
+
+
+
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(onPressed: () {
+                WebPage = 0;
+                Navigator.push(context, MaterialPageRoute(builder: (context) => MusicSearch()),);
+              }, icon: Icon(Icons.music_note)),
+              IconButton(onPressed: () {
+                WebPage = 1;
+              }, icon: Icon(Icons.dynamic_feed)),
+              IconButton(onPressed: () {
+                WebPage = 2;
+                Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()),);
+              }, icon: Icon(Icons.dvr_rounded)),
+            ],
           ),
         ),
+      ),
     );
   }
 }
